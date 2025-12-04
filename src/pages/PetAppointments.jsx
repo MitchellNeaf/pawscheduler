3e15a4c74e0accf5e339cde7830656c7ffb39392
MitchellNeaf <mitchellneaffer@gmail.com>
2025-12-03 21:31:16 -0500

    updating schedule

--- src/pages/PetAppointments.jsx
+++ src/pages/PetAppointments.jsx
@@ -1,7 +1,7 @@
 // src/pages/Schedule.jsx
 import React, { useEffect, useState } from "react";
 import { supabase } from "../supabase";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -863,7 +863,6 @@ export default function Schedule() {
   const [rebookModalOpen, setRebookModalOpen] = useState(false);
   const [rebookAppt, setRebookAppt] = useState(null);
 
-  const navigate = useNavigate();
 
   // Auth user
   useEffect(() => {
